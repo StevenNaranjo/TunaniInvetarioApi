89bfff89943436ab91d2faa4a1e12d6633d6bccc
Alex Naranjo <asnaranjom@gmail.com>
2024-07-02 09:57:53 -0600

    Cambios en el app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api", routes);
-
+app.use(cors());
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
